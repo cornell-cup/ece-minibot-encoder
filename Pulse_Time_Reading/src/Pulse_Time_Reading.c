2a932eb4eb2e8b5d00552d369bc9664a3416f72e
clc288 <Claire@clairecutersair.coecis.cornell.edu>
2015-11-20 00:29:11 -0500

    Measure Period of Pulse Nov20

--- Pulse_Time_Reading/src/Pulse_Time_Reading.c
+++ Pulse_Time_Reading/src/Pulse_Time_Reading.c
@@ -8,9 +8,18 @@
 void ISR_PULSEIN();
 long absoluteTime(void);
 
+float CPR = 64.0;
+float RPM;
+long prevTime;
+long currTime;
+float usecPerCount = 0;
+
+
 int main() {
 
-#define PULSEIN_PIN		7
+#define PULSEIN_PIN		8
+
+	currTime = 0;
 
 	// Initialize GPIO pin for input pulse reading
 	mraa_gpio_context PULSEIN = mraa_gpio_init(PULSEIN_PIN);
@@ -23,23 +32,25 @@ int main() {
 	mraa_gpio_dir(PULSEIN, MRAA_GPIO_IN);
 
 	// Attach ISR to pin
-	mraa_gpio_isr(PULSEIN, MRAA_GPIO_EDGE_RISING, ISR_PULSEIN, NULL);
+	mraa_gpio_isr(PULSEIN, MRAA_GPIO_EDGE_FALLING, ISR_PULSEIN, NULL);
 
 	fprintf(stdout, "Done configuring!\n");
 
-	long initial = absoluteTime();
 	while (1){
-		long t = absoluteTime() - initial;
-		fprintf(stdout, "%.1f\n", (float)t); // print in seconds
+		fprintf(stdout, "usec/count: %.1f   RPM: %.1f\n", (float)usecPerCount, (float)RPM); // print in seconds
 		sleep(1);
 	}
 
-  return 0;
+	return 0;
 }
 
 
 void ISR_PULSEIN(void* args){
-	// interrupt handler
+
+	prevTime = currTime;
+	currTime = absoluteTime();
+	usecPerCount = currTime - prevTime;
+	RPM = 1000000.0 * 60.0 * ((1/usecPerCount)/CPR);
 
 }
 
@@ -47,9 +58,8 @@ long absoluteTime(void){
 	// Return absolute time in us
 	struct timespec t;
 	clock_gettime(CLOCK_MONOTONIC, &t);
-//	return (t.tv_nsec/1000);
-	return ((long)t.tv_sec*1000);
-
+	//	return (t.tv_nsec/1000);
+	return ((long)t.tv_sec*1000+t.tv_nsec/1000);
 }
 
 
